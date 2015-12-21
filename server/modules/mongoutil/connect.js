6b1c2873b5cb30425cdf42273cd770cf49b8cd7b
Bharath Kumar Chennupati <BharathKumar.Chennupati@klm.com>
2015-12-22 00:27:28 +0100

    Added CURD operations to mongoUtil, used the mongoutil CURD operations for employee

--- server/modules/mongoutil/connect.js
+++ server/modules/mongoutil/connect.js
@@ -1,22 +1,36 @@
-var MongoClient = require( 'mongodb' ).MongoClient;
+var db = require('mongodb'); //var mangojs = require("mangojs");
+var ObjectID = db.ObjectID;
+var MongoClient = db.MongoClient;
 
 var _db;
 
-module.exports = function(dbName) {
+module.exports = function() {
 return {
 
   connectToServer : function( callback ) {
-    MongoClient.connect( "mongodb://localhost:27017/test" + dbName, function( err, db ) {
-      _db = db;
-      return callback( err );
-    } );
-  },
-
-  getDb: function() {
-    return _db;
-  }
+                        MongoClient.connect( "mongodb://localhost:27017/employees", function( err, db ) {
+                          _db = db;
+                           callback( err ); //return callback( err ); TODOD : What is the use of return here?
+                        } );
+                      },
+ getCollection : function (collectionName) {
+                    return _db.collection(collectionName);
+                 },
+  getDb : function() {
+            return _db;
+          },
+  getAllDocuments : function (collectionName) {
+                        return this.getCollection(collectionName).find({});
+                    },
+  insertOneDocument : function (collectionName, document) {
+                        return this.getCollection(collectionName).insertOne(document);
+                     },
+  deleteDocumentById : function (collectionName, id, callback) {
+                        this.getCollection(collectionName).deleteOne({_id : ObjectID(id)}, function() {
+                        callback (err, results)
+                        });
+                     }
 }
-
 };
 
 
