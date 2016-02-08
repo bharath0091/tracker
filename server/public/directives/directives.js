540b79f9aa133df38d563fe6463d8ef3f42f236c
Bharath Kumar Chennupati <bharath.chennupati@klm.com>
2016-02-08 11:56:15 +0100

    Minor fixes

--- server/public/directives/directives.js
+++ server/public/directives/directives.js
@@ -8,9 +8,9 @@ directivesModule.directive('actionRow', function($compile){
         link: function($scope , element){
             element.bind("click", function(e){
                 initializeAction($scope);
-                var childNode = $compile('<input ng-model="action.fields[' +counter +'].name" type="text">  <select ng-model="action.fields[' + counter + '].type"> <option value="text">text</option><option value="date">date</option></select> <br>')($scope);
+                var childNode = $compile('<input ng-model="action.fields[' +counter +'].name" type="text" placeholder="Enter new field name">  <select ng-model="action.fields[' + counter + '].type" ><option value="text" disabled selected>Select field type</option> <option value="text">text</option><option value="date">date</option></select> <br><br>')($scope);
                 counter++;
-                element.parent().append(childNode);
+                element.parent().prepend(childNode);
             });
             element.on('$destroy', function() {
                 counter = 0;
